544b7b1cde2260140a013c6fa81eeeb0ca10c52f
Sergey <xxsmotur@gmail.com>
2018-12-16 18:16:10 +0500

    Tmp solution for Where method

--- where.go
+++ where.go
@@ -1,14 +1,18 @@
 package linq
 
 import (
+	"fmt"
 	"reflect"
 )
 
 // Where provides filtering on collection
-func (l *Linq) Where(f func(d interface{}) bool) *Linq {
+func (l *Linq) Where(f interface{}) *Linq {
+	if !isFunction(f) {
+		panic("Where: input data not a function")
+	}
 	s := reflect.ValueOf(l.coll)
 	for i := 0; i < s.Len(); i++ {
-		f(s.Index(i).Interface())
+		fmt.Println(s.Index(i).Interface())
 	}
 	return l
 }

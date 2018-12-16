a359415c0115428d05dbd247861c6b4c1d64b391
Sergey <xxsmotur@gmail.com>
2018-12-16 17:57:00 +0500

    Check if input type is slice

--- linq.go
+++ linq.go
@@ -1,5 +1,12 @@
 package linq
 
+import (
+	"errors"
+	"reflect"
+)
+
+var errNoSlice = errors.New("input type not slice")
+
 // Enumerable defines main interface for collections
 type Enumerable interface {
 	GetList() []interface{}
@@ -12,8 +19,20 @@ type Linq struct {
 }
 
 // New creates init of the linq
-func New(data interface{}) *Linq {
+func New(data interface{}) (*Linq, error) {
+	ok := isSliceType(data)
+	if !ok {
+		return nil, errNoSlice
+	}
 	return &Linq{
 		coll: data,
+	}, nil
+}
+
+// isSliceType returns true if input data is slice
+func isSliceType(data interface{}) bool {
+	if reflect.TypeOf(data).Kind() == reflect.Slice {
+		return true
 	}
+	return false
 }

bdf80fce8dad3d2251fb1cfe8b2eb45e6e024319
Sergey <xxsmotur@gmail.com>
2018-12-16 18:22:47 +0500

    Checking of num on input args

--- where.go
+++ where.go
@@ -10,6 +10,15 @@ func (l *Linq) Where(f interface{}) *Linq {
 	if !isFunction(f) {
 		panic("Where: input data not a function")
 	}
+
+	numIn := reflect.TypeOf(f).NumIn()
+	if numIn != 1 {
+		panic("Where: input arguments is not 1")
+	}
+	outIn := reflect.TypeOf(f).NumOut()
+	if outIn != 1 {
+		panic("Where: output arguments is not 1")
+	}
 	s := reflect.ValueOf(l.coll)
 	for i := 0; i < s.Len(); i++ {
 		fmt.Println(s.Index(i).Interface())

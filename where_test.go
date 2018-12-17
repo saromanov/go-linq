d21df3e3c6aea13a947130205346e8b7f826bcef
Sergey <xxsmotur@gmail.com>
2018-12-17 18:15:35 +0500

    Add Test where multple

--- where_test.go
+++ where_test.go
@@ -19,6 +19,28 @@ func TestWhereIntBasic(t *testing.T) {
 	}
 }
 
+func TestWhereIntMultiple(t *testing.T) {
+	fp, _ := New([]int{2, 4, 5, 8, 15, 20})
+	result := fp.Where(func(x int) bool {
+		if x > 2 {
+			return true
+		}
+		return false
+	}).Where(func(x int) bool {
+		if x > 8 {
+			return true
+		}
+		return false
+	})
+	expected := []int64{15, 20}
+	resultInt := result.Result().([]int64)
+	for i, x := range expected {
+		if resultInt[i] != x {
+			t.Errorf("invalid response. Expected: %d. Found %d", x, resultInt[i])
+		}
+	}
+}
+
 func TestWhereFloatBasic(t *testing.T) {
 	fp, _ := New([]float64{2.5, 2.7, 2.9, 3.2})
 	result := fp.Where(func(x float64) bool {

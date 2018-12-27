8ce38e81dde662d102e522037c880ae8ffc8aed8
Sergey <xxsmotur@gmail.com>
2018-12-27 21:51:11 +0500

    Add methods for testing of the Last method

---
+++ last_test.go
@@ -0,0 +1,45 @@
+package linq
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLast(t *testing.T) {
+	l, err := New([]string{"Alfa Romeo", "Aston Martin", "Audi", "Nissan", "Chevrolet", "Chrysler", "Dodge", "BMW",
+		"Ferrari", "Bentley", "Ford", "Lexus", "Mercedes", "Toyota", "Volvo", "Subaru", "Ferrari"})
+	if err != nil {
+		panic(err)
+	}
+	result := l.Last(func(x string) bool {
+		return true
+	})
+	resp := result.Result().([]string)
+	assert.Equal(t, resp[0], "Ferrari", "strings should be equal")
+}
+
+func TestLastEmpty(t *testing.T) {
+	l, err := New([]string{})
+	if err != nil {
+		panic(err)
+	}
+	result := l.Last(func(x string) bool {
+		return true
+	})
+	assert.Nil(t, result.Result())
+}
+
+func TestLastWithPrefix(t *testing.T) {
+	l, err := New([]string{"Alfa Romeo", "Aston Martin", "Audi", "Nissan", "Chevrolet", "Chrysler", "Dodge", "BMW",
+		"Ferrari", "Bentley", "Ford", "Lexus", "Mercedes", "Toyota", "Volvo", "Subaru", "Жигули :)"})
+	if err != nil {
+		panic(err)
+	}
+	result := l.Last(func(x string) bool {
+		return strings.HasPrefix(x, "T")
+	})
+	resp := result.Result().([]string)
+	assert.Equal(t, resp[0], "Toyota", "strings should be equal")
+}

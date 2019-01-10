12cbb61739a0afc7b625f2cc216c7a8fdfb7dd61
Sergey <xxsmotur@gmail.com>
2019-01-11 00:54:21 +0500

    implementation of the Zip method

---
+++ zip.go
@@ -0,0 +1,28 @@
+package linq
+
+import "reflect"
+
+// Zip provides applying a specified function to the corresponding elements of
+// two sequences, producing a sequence of the results.
+func (l *Linq) Zip(coll interface{}, f interface{}) *Linq {
+	if !isFunction(f) {
+		panic("Zip: input data not a function")
+	}
+	size := len(l.result)
+	data := makeData(coll)
+	sizeInp := len(data)
+	length := size
+	if size > sizeInp {
+		length = sizeInp
+	}
+	tmp := l.result
+	l.result = l.result[:0]
+	for i := 0; i < length; i++ {
+		fResult := invoke2(f, data[i], tmp[i])
+		if len(fResult) == 0 {
+			continue
+		}
+		l.result = append(l.result, reflect.ValueOf(fResult[0].Interface()))
+	}
+	return l
+}
